d653596a61dbd2307f7ed93ba19ab955946ebfc1
hym <huangyiming@111.com.cn>
2021-12-02 19:17:07 +0800

    [FIX]fix

--- src/main/java/com/talone/udf/aliv/udtf/TKQMinPriceUDTF.java
+++ src/main/java/com/talone/udf/aliv/udtf/TKQMinPriceUDTF.java
@@ -31,16 +31,16 @@ public class TKQMinPriceUDTF extends UDTF {
 
     private static Map tkfx(Double xprice, String activityInfo, String tkqInfo) {
         Map map = new HashMap();
-        List<Integer> tkq = new ArrayList<Integer>();
-        Integer tke = 0;
-        Integer tkj = 0;
+        List<Double> tkq = new ArrayList<Double>();
+        Double tke = 0D;
+        Double tkj = 0D;
         tkqInfo = tkqInfo.replace(" ", "");
-        String yhqzz = ".*满\\d+(\\D|)+减\\d+.*";
+        String yhqzz = ".*满[0-9]+([.]{1}[0-9]+){0,1}+减[0-9]+([.]{1}[0-9]+){0,1}+.*";
         boolean yhqzzisMatch = Pattern.matches(yhqzz, tkqInfo);
         if (yhqzzisMatch) {
-            for (String sss : tkqInfo.replaceAll("[^0-9]", ",").split(",")) {
+            for (String sss : tkqInfo.replaceAll("[^0-9.]", ",").split(",")) {
                 if (sss.length() > 0)
-                    tkq.add(Integer.parseInt(sss));
+                    tkq.add(Double.parseDouble(sss));
             }
         }
 
@@ -66,6 +66,7 @@ public class TKQMinPriceUDTF extends UDTF {
             }
         }
 
+        String yhqzz2 = ".*满\\d+(\\D|)+减\\d+.*";
         if (null != activityInfo && !"".equals(activityInfo)) {
             activityInfo = activityInfo.replaceAll("\\\\n", "|");
             String[] json = activityInfo.split("\"body\"");
@@ -80,7 +81,7 @@ public class TKQMinPriceUDTF extends UDTF {
             for (String yhqstr : yhqstrss) {
                 // 满减
                 yhqstr = yhqstr.replace(" ", "");
-                boolean yhqzzisMatch1 = Pattern.matches(yhqzz, yhqstr);
+                boolean yhqzzisMatch1 = Pattern.matches(yhqzz2, yhqstr);
                 if (yhqzzisMatch1) {
                     for (String sss : yhqstr.replaceAll("[^0-9]", ",").split(",")) {
                         if (sss.length() > 0)
@@ -120,11 +121,11 @@ public class TKQMinPriceUDTF extends UDTF {
     public static void main(String[] args) {
         long s1 = System.currentTimeMillis();
         // 单价
-        double xprice = 77;
+        double xprice = 1597;
 
         // 优惠券规则解析
-        String yhqstrxx = "{\"head\": \"雅塑72粒 到2021-10-31 00:00:00结束\", \"body\": \" 满77元 减15元 \\n 满790元 减120元 \\n 满1700元 减400元 \"}";
-        String tkqInfo = "满74减25";
+        String yhqstrxx = "{\"head\": \"慕小腰日常满减 到2022-05-29 00:00:00结束\", \"body\": \" 满35.8元 减8元 \\n 满338元 减212元 \\n 满1014元 减642元 \\n 满1597元 减1012元 \"}";
+        String tkqInfo = "满19减8";
         Map map = tkfx(xprice, yhqstrxx, tkqInfo);
 
 
